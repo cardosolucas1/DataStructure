dc711879d309c17f39d4f72a02ce2db0bbd1aa41
Cardoso <lsc.cardosoo@gmail.com>
2020-11-10 11:19:02 -0300

    Criar funçao para criar a lista encadeada e imprimi-la

--- List/linkedList.c
+++ List/linkedList.c
@@ -3,16 +3,50 @@
 
 typedef struct LinkedList {
   int intValue;
+  struct LinkedList *head;
+  struct LinkedList *tail;
   struct LinkedList *nextNode;
   struct LinkedList *prevNode;
-} LinkedList;
+}LinkedList;
+
+LinkedList createLinkedList() {
+  LinkedList *list = (struct LinkedList *)malloc(sizeof(LinkedList));
+  list->head = NULL;
+  list->tail = NULL;
+  list->nextNode = NULL;
+  list->prevNode = NULL;
+  return *list;
+}
 
 
-// Encadear valor
-void chainValue(LinkedList list, int value) {
+//Encadear valor
+// void chainValue(LinkedList *list, int value) {
+//   if (list->head->intValue == NULL) {
+//     list->head->intValue = value;
+//   }
+//   if (list->nextNode->intValue == NULL) {
+//     list->nextNode->intValue == value;
+//   } else {
+//     chainValue(list, value);
+//   }
+// }
 
+void printList(LinkedList *list) {
+  if (list->head == NULL) {
+    printf("%s", "Empty List\n");
+    return;
+  } else {
+    printf("%s", "Lista deve ser impressa\n");
+  }
+  printf("%d ", list->intValue);
+  if (list->nextNode != NULL) {
+    printList(list->nextNode);
+  }
 }
 
 int main () {
+  LinkedList newList = createLinkedList();
+  printList(&newList);
+  // printf("%s", "Hello World\n");
   return 0;
 }
