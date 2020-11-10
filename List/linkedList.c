fb6c1d9656eb91745955370c5be7d5a8e79c25d6
Cardoso <lsc.cardosoo@gmail.com>
2020-11-10 10:17:09 -0300

    Inicializar Lista encadeada em C

---
+++ List/linkedList.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct LinkedList {
+  int intValue;
+  struct LinkedList *nextNode;
+  struct LinkedList *prevNode;
+} LinkedList;
+
+
+// Encadear valor
+void chainValue(LinkedList list, int value) {
+
+}
+
+int main () {
+  return 0;
+}
