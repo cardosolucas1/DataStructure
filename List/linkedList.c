f5b86b8d65f7b259d3c1eb2d3318f305c4db7150
Cardoso <lsc.cardosoo@gmail.com>
2020-11-10 11:56:26 -0300

    Reestruturar lista

--- List/linkedList.c
+++ List/linkedList.c
@@ -3,50 +3,39 @@
 
 typedef struct LinkedList {
   int intValue;
-  struct LinkedList *head;
-  struct LinkedList *tail;
-  struct LinkedList *nextNode;
-  struct LinkedList *prevNode;
+  struct LinkedList* head;
+  // struct LinkedList* tail;
+  struct LinkedList* nextNode;
+  // struct LinkedList* prevNode;
 }LinkedList;
 
-LinkedList createLinkedList() {
-  LinkedList *list = (struct LinkedList *)malloc(sizeof(LinkedList));
+LinkedList* createLinkedList() {
+  LinkedList* list = (struct LinkedList*)malloc(sizeof(LinkedList));
   list->head = NULL;
-  list->tail = NULL;
+  // list->tail = NULL;
   list->nextNode = NULL;
-  list->prevNode = NULL;
-  return *list;
+  // list->prevNode = NULL;
+  return list;
 }
 
-
 //Encadear valor
-// void chainValue(LinkedList *list, int value) {
-//   if (list->head->intValue == NULL) {
-//     list->head->intValue = value;
-//   }
-//   if (list->nextNode->intValue == NULL) {
-//     list->nextNode->intValue == value;
-//   } else {
-//     chainValue(list, value);
-//   }
-// }
+void chainValue(LinkedList *list, int value) {
+  list = (struct LinkedList*)malloc(sizeof(LinkedList));
+  list->intValue = value;
+  list->nextNode = NULL;
+}
 
-void printList(LinkedList *list) {
-  if (list->head == NULL) {
-    printf("%s", "Empty List\n");
-    return;
-  } else {
-    printf("%s", "Lista deve ser impressa\n");
-  }
-  printf("%d ", list->intValue);
-  if (list->nextNode != NULL) {
-    printList(list->nextNode);
+void printList(LinkedList* list) {
+  LinkedList* printThisList  = list;
+   printf("Head: %d\n", printThisList->intValue);
+  while (printThisList->nextNode != NULL) {
+    printf("%d ", printThisList->nextNode->intValue);
+    printThisList = printThisList->nextNode;
   }
 }
 
 int main () {
-  LinkedList newList = createLinkedList();
-  printList(&newList);
-  // printf("%s", "Hello World\n");
+  LinkedList* newList = createLinkedList();
+  printList(newList);
   return 0;
 }
